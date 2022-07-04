705c8008a15e3020689a1e82eba42d81d60a7d7c
OkazakiRui <okazaki200226@gmail.com>
2022-07-04 15:52:02 +0900

    feat: todo list

--- src/App.tsx
+++ src/App.tsx
@@ -1,45 +1,92 @@
-import { useState } from 'react'
-import logo from './logo.svg'
-import './App.css'
+import { ChangeEventHandler, FC, FormEventHandler, useState, VFC } from 'react';
 
-function App() {
-  const [count, setCount] = useState(0)
+type Task = {
+  task: string;
+  isCompleted: boolean;
+};
+
+const initialState = [
+  {
+    task: 'Learn vue.js',
+    isCompleted: false,
+  },
+  {
+    task: 'Learn React Hook',
+    isCompleted: false,
+  },
+  {
+    task: 'Learn Gatsby.js',
+    isCompleted: false,
+  },
+];
+
+const TodoList: FC = () => {
+  const [todoList, setTodoList] = useState<Task[]>(initialState);
+  const [task, setTask] = useState('');
+
+  const handleNewTask: ChangeEventHandler<HTMLInputElement> = (e) => {
+    setTask(e.target.value);
+  };
+
+  const handleSubmit: FormEventHandler<HTMLFormElement> = (e) => {
+    e.preventDefault();
+    if (task === '') return;
+    setTodoList((todoList) => [...todoList, { task, isCompleted: false }]);
+    setTask('');
+  };
+
+  const handleRemoveTask = (index: number) => {
+    const newTodos = [...todoList];
+    newTodos.splice(index, 1);
+    setTodoList(newTodos);
+  };
+
+  const handleUpdateTask = (index: number) => {
+    const newTodos = todoList.map((todo, todoIndex) => {
+      if (todoIndex === index) {
+        todo.isCompleted = !todo.isCompleted;
+      }
+      return todo;
+    });
+    setTodoList(newTodos);
+  };
 
   return (
-    <div className="App">
-      <header className="App-header">
-        <img src={logo} className="App-logo" alt="logo" />
-        <p>Hello Vite + React!</p>
-        <p>
-          <button type="button" onClick={() => setCount((count) => count + 1)}>
-            count is: {count}
-          </button>
-        </p>
-        <p>
-          Edit <code>App.tsx</code> and save to test HMR updates.
-        </p>
-        <p>
-          <a
-            className="App-link"
-            href="https://reactjs.org"
-            target="_blank"
-            rel="noopener noreferrer"
+    <div>
+      <h1>ToDo List</h1>
+      <form onSubmit={handleSubmit}>
+        Add Task :
+        <input
+          value={task}
+          placeholder="Add New Task"
+          onChange={handleNewTask}
+        />
+      </form>
+      <ul>
+        {todoList.map((todo, index) => (
+          <li
+            key={index}
+            style={{
+              textDecoration: todo.isCompleted ? 'line-through' : 'none',
+            }}
           >
-            Learn React
-          </a>
-          {' | '}
-          <a
-            className="App-link"
-            href="https://vitejs.dev/guide/features.html"
-            target="_blank"
-            rel="noopener noreferrer"
-          >
-            Vite Docs
-          </a>
-        </p>
-      </header>
+            <input
+              type="checkbox"
+              checked={todo.isCompleted}
+              onChange={() => handleUpdateTask(index)}
+            />
+            {todo.task}
+            <span
+              onClick={() => handleRemoveTask(index)}
+              style={{ marginLeft: 10, cursor: 'pointer', color: 'red' }}
+            >
+              [X]
+            </span>
+          </li>
+        ))}
+      </ul>
     </div>
-  )
-}
+  );
+};
 
-export default App
+export default TodoList;
